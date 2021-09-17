18f2066e2e59facdbc404a5aadb5d9b7cdc6ce23
sahil <sahilpatel7998.sp@gmail.com>
2021-09-17 19:04:44 +0530

    17-sep

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
+import image from "../src/images/user.png"
 
-function App() {
+const App = () => {
+
+  const [inputData, setInputData] = useState("");
+
+
+  const [items, setItems] = useState([])
+
+  const addItem = () => {
+
+    if (!inputData) {
+
+      alert('The item can not be empty!')
+
+    } else {
+
+      setItems([...items, inputData])
+      setInputData('')
+
+    }
+
+
+    // const addItem = () =>{
+    //   setItems([...items, inputData])
+
+  }
+
+  const deleteItem = (id) => {
+    console.log(id)
+
+    const updatedItems = items.filter((el, index) => {
+      return index !== id;
+    });
+
+    setItems(updatedItems)
+
+  }
+
+  const removeAll = () => {
+    setItems([]);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="main_div">
+
+        <div className="second_div">
+
+          <figure>
+            <img src = {image} className = "user-IMG" alt = "User"/>
+            <figcaption> Update your info here!</figcaption>
+          </figure>
+
+          <div className="addItems">
+            <input
+              type="text"
+              placeholder="Enter your items"
+              value={inputData}
+              onChange={(e) => { setInputData(e.target.value) }}
+            />
+
+            <i className="fa fa-plus add-btn" title="add item" onClick={addItem}></i>
+          </div>
+
+          <div className="showItems">
+
+            {
+              items.map((el, index) => {
+                return (
+                  <div className="eachItems" key={index}>
+                    <p>{el}</p>
+                    <button className="btn" title="delete item" onClick={() => deleteItem(index)}> - </button>
+                  </div>
+                );
+              })
+            }
+
+
+          </div>
+
+          <div className="showItems">
+            <button onClick={removeAll}> Remove items</button>
+          </div>
+
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default App;
+
